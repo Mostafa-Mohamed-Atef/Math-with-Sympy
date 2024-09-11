44113d943a45d88b66a98d5305760cc8b84a8a61
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-09-11 20:22:04 +0300

    add system equations

--- find x.py
+++ find x.py
@@ -1,4 +1,12 @@
 from sympy import *
-l, r = input("your equation: \n").strip().split("=")
-eqx = Eq(sympify(l),sympify(r))
-print(solve(eqx))
+x, y, z = symbols('x y z')
+num_of_equations = int(input("Enter Number of Equations: \n"))
+equations = []
+for _ in range(num_of_equations):
+    equation = input("Enter your equation (e.g., 'x + y = 5'): \n").strip()
+    left, right = equation.split("=")
+    eq = Eq(sympify(left), sympify(right))
+    equations.append(eq)
+
+solution = solve(equations)
+print(solution)
